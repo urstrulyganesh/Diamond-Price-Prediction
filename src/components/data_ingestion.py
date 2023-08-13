9eb36b9b58601dcde716a21357249c0fbc45a09d
urstrulyganesh <ganeshawarde2@gmail.com>
2023-08-13 10:36:08 +0530

    Ingestion and Transformatin

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info('Data Ingestion methods Starts')
         try:
-            df=pd.read_csv(os.path.join('../notebooks/data','gemstone.csv'))
+            df=pd.read_csv('notebooks/data/gemstone.csv')
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
@@ -49,3 +49,8 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
+"""if __name__=='__main__':
+	obj=DataIngestion()
+	train_data_path,test_data_path=obj.initiate_data_ingestion()
+	data_transformation =DataTransformation()
+	train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)"""
